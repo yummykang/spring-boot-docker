cbdb510ef0c5858ce38f0cf82901bd6b9c2db47f
wek <123456>
2017-03-28 10:52:46 +0800

    Initial commit

---
+++ src/main/java/me/yummykang/SpringBootDockerApplication.java
@@ -0,0 +1,20 @@
+package me.yummykang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootDockerApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "Hello docker world";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDockerApplication.class, args);
+	}
+}
